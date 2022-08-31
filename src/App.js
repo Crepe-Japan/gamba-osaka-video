99aa96361fc8ad403e83762f5415dbf8eb40cfe9
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2022-08-31 16:55:36 +0900

    add: front page template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { Box, Image, VStack } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className='App'>
+      < VStack >
+        <Box py='4'>
+          <Image src="assets\images\renewal_title.png" alt="renewal-title" width="100%" height="auto" />
+          <Image src="assets\images\title_text.png" alt="title" width="100%" />
+        </Box>
+        <Box px='6'>
+          <VStack p='4' id="card"  >
+            <Box>
+              <Image onClick={() => alert("Hello")} className='captureBtn' src="assets\images\photo_button.png" alt="renewal-title" />
+            </Box>
+            <Box>
+              <img src="assets\images\modelview_text.png" alt="model-view-text" width="80%"
+                style={{ margin: "auto", display: "block" }} />
+            </Box>
+          </VStack>
+        </Box>
+        <Box p="6">
+          <a href="/" target="_blank" rel="noreferrer">
+            <div style={{ textAlign: "center" }} >
+              <img src="assets\images\renewal-img.jpg" alt="Renewal" width="100%" />
+            </div>
+          </a>
+        </Box>
+      </VStack >
+    </Box >
+
   );
 }
 
