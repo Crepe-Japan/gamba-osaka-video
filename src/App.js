416dc4f13f90e74a95597ca4b8f4a9c2f2653f4a
ppkSuntex <pyaephyokyaw@suntex-jp.com>
2022-09-09 22:35:07 +0900

    commit: revert to stable

--- src/App.js
+++ src/App.js
@@ -7,43 +7,12 @@ import {
 import VideoCapture from './components/VideoCapture'
 import Home from './components/Home';
 function App() {
-  const videoJsOptions = {
-    controls: false,
-    bigPlayButton: false,
-    width: 2,
-    height: 2,
-    fluid: false,
-    plugins: {
-      record: {
-        audio: true,
-        /*   video: true, */
-        video: {
-          // video media constraints: set resolution of camera
-          width: { min: 1280, ideal: 640, max: 1920 },
-          height: { min: 920, ideal: 480, max: 1080 }
-        },
-        // dimensions of captured video frames
-        frameWidth: 1920,
-        frameHeight: 1080,
-        maxLength: 10,
-        debug: true,
-        /*     // enable ffmpeg.wasm plugin
-            convertEngine: 'ffmpeg.wasm',
-            convertWorkerURL: '../../node_modules/@ffmpeg/core/dist/ffmpeg-core.js',
-            // convert recorded data to MP4 (and copy over audio data without encoding)
-            convertOptions: ['-c:v', 'libx264', '-preset', 'slow', '-crf', '22', '-c:a', 'copy', '-f', 'mp4'],
-            // specify output mime-type
-            pluginLibraryOptions: {
-              outputType: 'video/mp4'
-            } */
-      }
-    }
-  };
+
   return (
     <Router>
       <Routes>
         <Route exact path='/' element={< Home />}></Route>
-        <Route path='/video-capture' element={< VideoCapture {...videoJsOptions} />}></Route>
+        <Route path='/video-capture' element={< VideoCapture />}></Route>
       </Routes>
     </Router>
   );
